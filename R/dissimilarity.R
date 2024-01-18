010574984a033c679499c75188dd4c189a6fa86c
Shi Quan <quan.shi@bio.ku.dk>
2024-01-18 15:40:47 +0100

    modified:   NAMESPACE 	renamed:    R/AlternativeExpression.R -> R/alt_exp.R 	new file:   R/cov.R 	new file:   R/dissimilarity.R 	renamed:    R/mergeMatrix.R -> R/matrix.R 	new file:   R/moransi.R 	modified:   R/preprocess.R 	renamed:    R/plot_cov.R -> R/vis.R 	new file:   R/weights.R 	modified:   man/GetWeights.Rd 	modified:   man/RunAutoCorr.Rd 	modified:   man/SetAutoCorrFeatures.Rd 	deleted:    man/WeightLineage.Rd 	modified:   src/cor_test.c

---
+++ R/dissimilarity.R
@@ -0,0 +1,181 @@
+#' @importFrom Matrix sparseMatrix
+#' @export
+RunBlockCorr <- function(object = NULL,
+                         bind.name = "gene_name",
+                         features = NULL,
+                         assay = NULL,
+                         bind.assay = NULL,
+                         bind.features = NULL,
+                         prefix = NULL,
+                         #cells = NULL,
+                         #order.cells = NULL,
+                         feature.types = NULL,                         
+                         min.features.per.block = 2,
+                         scale.factor = 1e4,
+                         weight.matrix.name = "WeightMatrix",
+                         #reduction = "pca",
+                         mode = 1,
+                         #sensitive.mode = FALSE,
+                         #spatial = FALSE,
+                         #W = NULL,
+                         cell.size = NULL,
+                         #dims = NULL,
+                         #weight.matrix = "RNA_nn",
+                         #k.nn = 9,
+                         perm=100,
+                         threads = 0,
+                         block.name = NULL,
+                         block.assay = NULL,
+                         block.features = NULL
+                         )
+{
+  if (!is.null(block.name)) {
+    warning(paste0("'block.name' is deprecated. Set bind.name = '", block.name, "'"))
+    bind.name <- block.name
+  }
+
+  if (!is.null(block.assay)) {
+    warning(paste0("'block.assay' is deprecated. Set bind.assay = '", block.assay, "'"))
+    bind.assay <- block.assay
+  }
+
+  if (!is.null(block.features)) {
+    warning(paste0("'block.features' is deprecated. Set bind.features = '", block.features, "'"))
+    bind.features <- block.features
+  }
+
+  if (weight.matrix.name %ni% names(object)) {
+    stop("No weight matrix found. Perform RunAutoCorr() first.")
+  }
+  
+  tt <- Sys.time()
+  #cells <- cells %||% order.cells
+  #cells <- cells %||% colnames(object)
+  #cells <- intersect(cells, colnames(object))
+  
+  assay <- assay %||% DefaultAssay(object)
+  message(paste0("Working on assay ", assay))
+
+  prefix <- prefix %||% bind.name
+  
+  features <- features %||% AutoCorrFeatures(object)
+  features <- intersect(features, rownames(object))
+
+  message(paste0("Working on ", length(features), " features."))
+
+  threads <- getCores(threads)
+
+  W <- object[[weight.matrix.name]]
+  
+  tab <- object[[assay]]@meta.features
+
+  if (bind.name %ni% colnames(tab)) {
+    stop(paste0("No bind.name found in the feature table of assay ", assay, ". Run LoadEPTanno or LoadVARanno first."))
+  }
+  
+  tab <- tab[tab[[bind.name]] != ".",] # skip unannotated records
+  if (!is.null(feature.types) & "type" %in% colnames(tab)) {
+    tab <- subset(tab, type %in% feature.types)
+  }
+  
+  blocks <- names(which(table(tab[[bind.name]]) >= min.features.per.block))
+
+  bind.features <- bind.features %||% blocks
+  blocks <- intersect(bind.features, blocks)
+  
+  features <- intersect(features, rownames(tab))
+    
+  if (length(features) == 0) {
+    stop("No features found.")
+  }
+
+  idx <- match(features, rownames(tab))
+  tab0 <- tab[idx,]
+  blocks <- intersect(unique(tab0[[bind.name]]), blocks)
+  
+  message(paste0("Processing ", length(blocks), " blocks.."))
+  tab <- subset(tab, tab[[bind.name]] %in% blocks)
+  
+  bind.assay <- bind.assay %||% "tmp.assay"
+
+  x <- GetAssayData(object, assay = assay, slot = "counts")
+  
+  # cell sizes
+  cs <- cell.size %||% colSums(x)  
+  #cells <- intersect(cells,names(which(cs > 0)))
+  #W <- W[cells, cells]
+
+  cells <- colnames(W)  
+  ncell <- length(cells)
+
+  x <- x[,cells]
+  cs <- cs[cells]
+  
+  ## all.features <- rownames(tab)
+  if (bind.assay %ni% names(object)) {
+    message("Aggregate counts..")
+    x0 <- x[rownames(tab),cells]
+    x0 <- as(x0, "TsparseMatrix")
+    
+    # Aggregate features in the same block
+    y <- sparseMatrix(i = match(tab[[bind.name]][x0@i+1], blocks),
+                      j = x0@j+1,
+                      x = x0@x, dims=c(length(blocks), length(cells)))
+
+    rm(x0)
+    rownames(y) <- blocks
+    colnames(y) <- cells
+  } else {
+    message(paste0("Trying to retrieve data from assay ", bind.assay,".."))
+    old.assay <- DefaultAssay(object)
+    DefaultAssay(object) <- bind.assay
+    blocks <- intersect(blocks, rownames(object))
+    tab <- subset(tab, tab[[bind.name]] %in% blocks)
+
+    y <- GetAssayData(object, assay = bind.assay, slot = "counts")#[blocks,cells]
+
+    DefaultAssay(object) <- old.assay
+    y <- y[,cells]
+  }
+
+  features <- intersect(features, rownames(tab))
+  tab <- tab[features,]
+  bidx <- match(tab[[bind.name]],rownames(y))
+  idx <- match(features, rownames(x))
+  #cidx <- match(cells, colnames(x))
+  
+  message(paste0("Test dissimlarity of binding features with ", threads, " threads."))
+  gc()
+  ta <- .Call("D_test", x, y, W, perm, threads, idx, bidx, cs, scale.factor, mode);
+  if (length(ta) == 1) stop(ta[[1]])
+
+  Lx <- ta[[1]]
+  Ly <- ta[[2]]
+  r <- ta[[3]]
+  e <- ta[[4]]
+  tval <- ta[[5]]
+
+  names(Lx) <- features
+  names(Ly) <- features
+  names(r) <- features
+  names(e) <- features
+  
+  pval <- pt(tval, df = perm - 1, lower.tail = FALSE)
+  names(pval) <- features
+  #padj <- p.adjust(pval, "BH")
+  #names(padj) <- features
+  tab <- object[[assay]]@meta.features
+  tab[[paste0(prefix, ".D")]] <- e[rownames(object)]
+  tab[[paste0(prefix, ".r")]] <- r[rownames(object)]
+  tab[[paste0(prefix, ".pval")]] <- pval[rownames(object)]
+  #tab[[paste0(prefix, ".padj")]] <- padj[rownames(object)]
+  object[[assay]]@meta.features <- tab
+
+  rm(ta)
+  gc()
+
+  tt <- Sys.time()-tt
+  
+  message(paste0("Runtime : ",format(tt)));
+  object
+}
--- R/vis.R
+++ R/vis.R
@@ -1,154 +1,107 @@
-#'@useDynLib Yano depth2matrix
+#'@importFrom dplyr %>%
+#'@importFrom gtools mixedsort
+#'@importFrom ggrepel geom_label_repel
+#'@import ggplot2
+#'@import ggrepel
+#'@importFrom scattermore geom_scattermore
+#' 
 #'@export
-bamcov <- function(bamfile = NULL, chr = NULL, start = -1, end = -1, strand = "both", split.bc = FALSE, cell.group = NULL, bin=1000, cell.tag = "CB", umi.tag = "UB")
+FbtPlot <- function(object = NULL, assay = NULL, chr = "chr", start = "start", val = NULL, col.by = NULL, cols = NULL, sel.chrs = NULL, xlab = "Chromosome", ylab = expression(-log[10](P)), raster = NULL, types = NULL, point.label = NULL, arrange.type = FALSE, label.size=3,...)
 {
-  if (is.null(bamfile)) stop("No BAM.")
-  if (is.null(chr)) stop("No chromosome name")
-  if (start == -1) stop("Start position.")
-  if (end == -1) stop("End position")
-
-  len <- end - start
-  if (len > 10000000) { ## 10M
-    stop("Too large region, try to shrink it before querying.")
-  }
-
-  win <- 1
-  if (len > bin*2) {
-    win <- as.integer(len/bin)
-  }
+  assay <- assay %||% DefaultAssay(object)
+  tab0 <- object[[assay]]@meta.features
+  cols <- cols %||% c("#131313","blue","#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A","#FFFF99","#B15928")
   
-  #bamfile <- normalizePath(bamfile)
-  message(paste0("Process ", bamfile))
+  if (is.null(val)) stop("No value name specified.")  
+  if (chr %ni% colnames(tab0)) stop("No chr name found.")
+  if (start %ni% colnames(tab0)) stop("No start name found.")
+  if (val %ni% colnames(tab0)) stop("No val name found.")
+  
+  if (!is.null(sel.chrs)) {
+    tab0 <- tab0 %>% filter (chr %in% sel.chrs)
+  }
 
-  strand.flag <- -1
-  if (strand == "forward") strand.flag <- 0
-  else if(strand == "reverse") strand.flag <- 1
-  else if (strand == "ignore") strand.flag <- -2
+  if (!is.null(types)) {
+    if ("type" %ni% colnames(tab0)) stop("No type found.")
+    tab0 <- subset(tab0, type %in% types)
+    if (nrow(tab0) == 0) stop("Empty records.")
+  }
+  lv <- mixedsort(unique(tab0[[chr]]))
+  
+  tab <- data.frame(chr = factor(tab0[[chr]], levels = lv),
+                    start = as.numeric(tab0[[start]]),
+                    pval = -log10(as.numeric(tab0[[val]])),
+                    row.names = rownames(tab0))
+  
+  if (!is.null(col.by)) {
+    tab[[col.by]] <- tab0[[col.by]]
+  }
 
-  if (!is.null(cell.group)) {
-    #if (length(cell.group)) stop("cell.group should be a vector of cell groups with cell names.")    
-    cell.names <- names(cell.group)
-    if (is.null(cell.names)) stop("Names of cell.group should not be empty.")
-    cell.group <- as.character(cell.group)
-    
-    groups <- levels(cell.group) %||% unique(cell.group)
-    group.ids <- match(cell.group, groups)
-    n <- length(cell.group)
-    split.bc <- TRUE    
-    dlst <- .Call("depth2matrix", bamfile, chr, start, end, strand.flag, split.bc, cell.tag, umi.tag, 20, cell.names, n, group.ids, groups)
+  tab <- subset(tab, !is.na(pval))
+  
+  data_cum <- tab %>% group_by(chr) %>% summarise(max_bp = max(start)) %>%
+    mutate(bp_add = lag(cumsum(max_bp), default = 0)) %>% select(chr, bp_add)
+  
+  data <- tab %>% inner_join(data_cum, by = "chr") %>% mutate(bp_cum = start + bp_add)
+  axis_set <- data %>% group_by(chr) %>% summarize(center = mean(bp_cum))
+
+  data$name <- rownames(tab)
+  rownames(data) <- rownames(tab)
+  
+  if (isTRUE(arrange.type)) data <- data %>% arrange(type)
+
+  fbt_theme <- function() {
+    theme(
+      legend.text = element_text(face = "italic",color = "black",family = "Helvetica",size = rel(1.5)),
+      axis.title.y = element_text(color = "black", family = "Helvetica",size = rel(1)),
+      axis.title.x = element_text(color = "black", family = "Helvetica",size = rel(1.5)),
+      axis.text = element_text(family = "Helvetica",color = "black",size = rel(1.5)),
+      axis.line = element_blank(),
+      axis.ticks = element_line(size = rel(1), color = "black"),
+      panel.border = element_rect(color = "black", fill = NA, size= rel(2), linetype = "solid"),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_rect(fill = "whitesmoke"),
+      legend.key = element_rect(fill = "whitesmoke"),
+      legend.title = element_text(size = rel(1.5),family = "Helvetica"),
+      plot.title = element_text(color = "black",face = "bold",size = rel(1.7),family = "Helvetica")
+    )
+  }
+
+  xi <- data_cum$bp_add
+  xi <- xi[-1]
+  p <- ggplot(data) + geom_vline(xintercept = xi, color="red", linetype="dotted")
+
+  raster <- raster %||% (nrow(x = data) > 1e5)
+  
+  if (!is.null(col.by)) {
+    if (raster) {
+      p <- p + geom_scattermore(mapping = aes(x=bp_cum, y = pval, col = .data[[col.by]]),shape=21,...)
+      p <- p + scale_color_manual(values = cols)
+    } else {
+      p <- p + geom_point(aes(x=bp_cum, y=pval, fill=.data[[col.by]]), shape=21,...)
+      p <- p + scale_fill_manual(values = cols)
+    }
   } else {
-    dlst <- .Call("depth2matrix", bamfile, chr, start, end, strand.flag, split.bc, cell.tag, umi.tag, 20, NULL, 0, NULL, NULL)
+    if (raster) {
+      p <- p + geom_scattermore(aes(x=bp_cum, y=pval),  ...)    
+    } else {
+      p <- p + geom_point(aes(x=bp_cum, y=pval),  ...)    
+    }
   }
+  p <- p + scale_x_continuous(label = axis_set$chr, breaks = axis_set$center,
+                              limits = c(min(data$bp_cum), max(data$bp_cum)),
+                              expand=c(0,0))
+  p <- p + fbt_theme() + theme(axis.title.y = element_text(size = rel(1.5), angle = 90))
+  p <- p + xlab(xlab) + ylab(ylab)
 
-  if (is.null(dlst)) stop("No cell found in the bam. Check cell names.")
-  
-  idx.0 <- which(dlst[[2]] == 0)
-  idx.1 <- which(dlst[[2]] == 1)
-
-  dlst[[1]] <- as.integer(dlst[[1]]/win)*win
-
-  x <- seq(as.integer(start/win)*win, as.integer(end/win)*win, win)
-  y <- unique(dlst[[4]])
-
-  tab <- data.frame()
-  
-  nr <- length(x)
-  nc <- length(y)
-  
-  if (length(idx.0) > 0) {
-    m0 <- Matrix::sparseMatrix(
-      i = match(dlst[[1]][idx.0], x),
-      j = match(dlst[[4]][idx.0], y),
-      x = dlst[[3]][idx.0],
-      dims=c(nr,nc))
-
-    rownames(m0) <- x
-    colnames(m0) <- y
-    m0 <- as.matrix(m0)
-    tab1 <- reshape2::melt(m0)
-    tab1$strand <- '+'
-    tab <- rbind(tab,tab1)
+  if (!is.null(point.label)) {
+    sel <- intersect(point.label, rownames(data))
+    if (length(sel) > 0) {
+      p <- p + geom_label_repel(data=data[sel,],aes(x=bp_cum, y=pval,label=name),box.padding = 0.5, max.overlaps = Inf, size=label.size)
+    }
   }
-
-  if (length(idx.1) > 0) {
-    m1 <- Matrix::sparseMatrix(
-      i = match(dlst[[1]][idx.1], x),
-      j = match(dlst[[4]][idx.1], y),
-      x = dlst[[3]][idx.1],
-      dims=c(nr,nc))
-
-    rownames(m1) <- x
-    colnames(m1) <- y
-    m1 <- as.matrix(m1)
-    tab2 <- reshape2::melt(m1)
-    tab2$strand <- '-'
-    tab <- rbind(tab,tab2)
-  }
-
-  colnames(tab) <- c("pos","label","depth","strand")
-  tab$depth <- as.integer(tab$depth/win)
-
-  return(tab)
-}
-#'@useDynLib Yano depth2matrix
-#'@export
-fragcov <- function(fragfile = NULL, chr = NULL, start = -1, end = -1, split.bc = FALSE, cell.group = NULL, bin=1000)
-{
-  if (is.null(fragfile)) stop("No fragment file.")
-  if (is.null(chr)) stop("No chromosome name")
-  if (start == -1) stop("No start position.")
-  if (end == -1) stop("No end position")
-
-  len <- end - start
-  if (len > 10000000) { ## 10M
-    stop("Too large region, try to shrink it before querying.")
-  }
-
-  win <- 1
-  if (len > bin*2) {
-    win <- as.integer(len/bin)
-  }
-  message(paste0("Process ", fragfile))
-
-  if (!is.null(cell.group)) {
-    #if (length(cell.group)) stop("cell.group should be a vector of cell groups with cell names.")    
-    cell.names <- names(cell.group)
-    if (is.null(cell.names)) stop("Names of cell.group should not be empty.")
-    cell.group <- as.character(cell.group)
-    
-    groups <- levels(cell.group) %||% unique(cell.group)
-    group.ids <- match(cell.group, groups)
-    n <- length(cell.group)
-    split.bc <- TRUE    
-    dlst <- .Call("fragment2matrix", fragfile, chr, start, end, cell.names, n, group.ids, groups)
-  } else {
-    dlst <- .Call("fragment2matrix", fragfile, chr, start, end,  NULL, 0, NULL, NULL)
-  }
-  
-  dlst[[1]] <- as.integer(dlst[[1]]/win)*win
-
-  x <- seq(as.integer(start/win)*win, as.integer(end/win)*win, win)
-  y <- unique(dlst[[4]])
-
-  nr <- length(x)
-  nc <- length(y)
-  
-  m0 <- Matrix::sparseMatrix(
-    i = match(dlst[[1]], x),
-    j = match(dlst[[4]], y),
-    x = dlst[[3]],
-    dims=c(nr,nc))
-
-  rownames(m0) <- x
-  colnames(m0) <- y
-  m0 <- as.matrix(m0)
-  tab <- reshape2::melt(m0)
-  tab$strand <- '.'
-
-  colnames(tab) <- c("pos","label","depth","strand")
-  tab$depth <- as.integer(tab$depth/win)
-
-  return(tab)
+  p
 }
 
 theme_cov <- function(...) {
@@ -517,26 +470,25 @@ plotTracks <-  function(bamfile=NULL, chr=NULL, start=NULL, end =NULL, gene=NULL
   return(p1 / p2 + plot_layout(heights=layout_heights[c(2,3)]))
 }
 
-#' @import RColorBrewer
-#' @importFrom pheatmap pheatmap
-#' @export
-plotModHeatmap <- function(lc = NULL,
-                           cluster_rows = FALSE,
-                           cluster_cols =FALSE,
-                           show_rownames = FALSE,
-                           show_colnames = FALSE,
-                           ...
-                           )
-{
-  ann <- lc$module
-  k <- length(unique(ann[['module']]))
-  
-  fig <- pheatmap(lc$LC,
-                  annotation_row = ann,
-                  cluster_rows = cluster_rows,
-                  cluster_cols = cluster_cols,
-                  show_rownames = show_rownames,
-                  show_colnames = show_colnames,
-                  ...)
-  fig
-}
+## #' @import RColorBrewer
+## #' @importFrom pheatmap pheatmap
+## #' @export
+## plotModHeatmap <- function(lc = NULL,
+##                            cluster_rows = FALSE,
+##                            cluster_cols =FALSE,
+##                            show_rownames = FALSE,
+##                            show_colnames = FALSE,
+##                            ...
+##                            )
+## {
+##   ann <- lc$module
+##   k <- length(unique(ann[['module']]))  
+##   fig <- pheatmap(lc$LC,
+##                   annotation_row = ann,
+##                   cluster_rows = cluster_rows,
+##                   cluster_cols = cluster_cols,
+##                   show_rownames = show_rownames,
+##                   show_colnames = show_colnames,
+##                   ...)
+##   fig
+## }
