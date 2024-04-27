5f3c8e26fe6b86be6c3fd0db98a1b47ebe114301
shiquan <shiquan@genomics.cn>
2024-04-27 18:22:10 +0200

    new file:   R/coclu.R 	new file:   R/zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,105 @@
+# codes edited from tidyverse/R/attach.R zzz.R
+core <- c("ggplot2", "Seurat", "dplyr")
+
+core_unloaded <- function() {
+  search <- paste0("package:", core)
+  core[!search %in% search()]
+}
+
+# Attach the package from the same package library it was
+# loaded from before. https://github.com/tidyverse/tidyverse/issues/171
+same_library <- function(pkg) {
+  loc <- if (pkg %in% loadedNamespaces()) dirname(getNamespaceInfo(pkg, "path"))
+  library(pkg, lib.loc = loc, character.only = TRUE, warn.conflicts = FALSE)
+}
+
+package_attach <- function() {
+  to_load <- core_unloaded()
+
+  suppressPackageStartupMessages(
+    lapply(to_load, same_library)
+  )
+
+  invisible(to_load)
+}
+
+#' @importFrom cli col_blue ansi_nchar rule style_bold symbol
+package_attach_message <- function(to_load) {
+  if (length(to_load) == 0) {
+    return(NULL)
+  }
+
+  header <- cli::rule(
+    left = cli::style_bold("Attaching packages"),
+    right = paste0("Yano ", package_version_h("Yano"))
+  )
+
+  to_load <- sort(to_load)
+  versions <- vapply(to_load, package_version_h, character(1))
+
+  packages <- paste0(
+    cli::col_green(cli::symbol$tick), " ", cli::col_blue(format(to_load)), " ",
+    cli::ansi_align(versions, max(cli::ansi_nchar(versions)))
+  )
+
+  if (length(packages) %% 2 == 1) {
+    packages <- append(packages, "")
+  }
+  col1 <- seq_len(length(packages) / 2)
+  info <- paste0(packages[col1], "     ", packages[-col1])
+
+  paste0(header, "\n", paste(info, collapse = "\n"))
+}
+
+package_version_h <- function(pkg) {
+  highlight_version(utils::packageVersion(pkg))
+}
+
+#' @importFrom cli col_red
+highlight_version <- function(x) {
+  x <- as.character(x)
+
+  is_dev <- function(x) {
+    x <- suppressWarnings(as.numeric(x))
+    !is.na(x) & x >= 9000
+  }
+
+  pieces <- strsplit(x, ".", fixed = TRUE)
+  pieces <- lapply(pieces, function(x) ifelse(is_dev(x), cli::col_red(x), x))
+  vapply(pieces, paste, collapse = ".", FUN.VALUE = character(1))
+}
+
+
+package_packages <- function(include_self = TRUE) {
+  raw <- utils::packageDescription("Yano")$Imports
+  imports <- strsplit(raw, ",")[[1]]
+  parsed <- gsub("^\\s+|\\s+$", "", imports)
+  names <- vapply(strsplit(parsed, "\\s+"), "[[", 1, FUN.VALUE = character(1))
+
+  if (include_self) {
+    names <- c(names, "Yano")
+  }
+
+  names
+}
+
+invert <- function(x) {
+  if (length(x) == 0) return()
+  stacked <- utils::stack(x)
+  tapply(as.character(stacked$ind), stacked$values, list)
+}
+
+.onAttach <- function(...) {
+  attached <- package_attach()
+  if (!is_loading_for_tests()) {
+    rlang::inform(package_attach_message(attached),class = "packageStartupMessage")
+  }
+}
+
+is_attached <- function(x) {
+  paste0("package:", x) %in% search()
+}
+
+is_loading_for_tests <- function() {
+  !interactive() && identical(Sys.getenv("DEVTOOLS_LOAD"), "Yano")
+}
