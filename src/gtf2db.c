eab05e40835dc004b28185d07a35c70e89872450
Shi Quan <quan.shi@bio.ku.dk>
2024-07-04 20:11:06 +0200

    modified:   NAMESPACE 	modified:   R/AltEXP.R 	modified:   R/gtf2db.R 	modified:   src/cor_test.c 	modified:   src/gtf2db.c

--- src/gtf2db.c
+++ src/gtf2db.c
@@ -13,13 +13,14 @@ _finalizer(SEXP ext)
     gtf_destroy(G);
 }
 
-SEXP gtf2db(SEXP filename)
+SEXP gtf2db(SEXP filename, SEXP utr)
 {
     if (!Rf_isString(filename)) error_return("Input is not String");
     const char *file = translateChar(STRING_ELT(filename, 0));
 
+    int use_utr = asInteger(utr);
     fprintf(stderr, "%s\n", file);
-    struct gtf_spec *G = gtf_read(file, 1);
+    struct gtf_spec *G = gtf_read(file, use_utr?1:2);
 
     SEXP ext = PROTECT(R_MakeExternalPtr(G, R_NilValue, R_NilValue));
     R_RegisterCFinalizerEx(ext, _finalizer, TRUE);
