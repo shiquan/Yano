7e4c5c1bcd8c56d8a6f49dde9096433eed8586c8
Shi Quan <quan.shi@bio.ku.dk>
2025-06-09 22:36:36 +0800

    modified:   NAMESPACE 	modified:   R/different_expression.R 	new file:   R/imputation.R 	modified:   man/FindDEP.Rd 	modified:   src/imputation.c

---
+++ R/imputation.R
@@ -0,0 +1,41 @@
+#' @title ImputationByWeight
+#' @param X Expression counts.
+#' @param cells Cells to imputate.
+#' @param W Weight matrix.
+#' @export
+ImputationByWeight <- function(X = NULL, cells = NULL, W = NULL)
+{
+  if (is.null(X) || is.null(W)) {
+    stop("X and/or W is not set.")
+  }
+
+  if ("dgCMatrix" %ni% class(X)) {
+    X <- as(X, "CsparseMatrix")
+  }
+  if ("dgCMatrix" %ni% class(W)) {
+    W <- as(x, "CsparseMatrix")
+  }
+
+  wcells <- colnames(W)
+  cells0 <- colnames(X)
+  cells1 <- intersect(cells0, wcells)
+  if (length(cells1) != length(wcells)) {
+    stop("Weight matrix has cells not in X.")
+  }
+  if (length(cells0) > length(cells1)) {
+    X <- X[,cells1]
+  }
+  
+  if (is.null(cells)) {
+    cells <- colnames(X)
+  }
+
+  idx <- match(cells, colnames(X))
+  names(idx) <- cells
+  idx <- sort(idx)
+  new.cells <- names(idx)
+  X0 <- .Call("imputation1", X, idx, W)
+  rownames(X0) <- rownames(X)
+  colnames(X0) <- new.cells
+  X0[,cells]
+}
--- src/imputation.c
+++ src/imputation.c
@@ -8,17 +8,29 @@
 
 static cholmod_common c;
 
+int scatter(CHM_SP A, int j, double beta, int *w, double *x, int mark, int *yi, int nz)
+{
+    int i, p;
+    int *Ap = A->p;
+    int *Ai = A->i;
+    double *Ax = A->x;
+    for (p = Ap[j]; p < Ap[j+1]; ++p) {
+        i = Ai[p];
+        if (w[i] < mark) { // init w and x
+            w[i] = mark;
+            yi[nz++] = i;
+            x[i] = beta*Ax[p];
+        } else {
+            x[i] += beta*Ax[p];
+        }
+    }
+    return nz;
+}
 SEXP imputation1(SEXP _x, SEXP idx, SEXP _W)
 {
     CHM_SP x = AS_CHM_SP__(_x);
     CHM_SP W = AS_CHM_SP__(_W);
     if (W->stype) return mkString("W cannot be symmetric");
-
-    CHM_SP t = M_cholmod_transpose(x, (int)x->xtype, &c);
-
-    const int * tp = (int*)t->p;
-    const int * ti = (int*)t->i;
-    const double *tx = (double*)t->x;
 
     const int * xp = (int*)x->p;
     const int * xi = (int*)x->i;
@@ -34,47 +46,37 @@ SEXP imputation1(SEXP _x, SEXP idx, SEXP _W)
     int nfeature = x->nrow;
     
     int n = 0, m = 10000;
+    if (m < nfeature) m = nfeature;
     int *yi = malloc(m*sizeof(int));
     double *yx = malloc(m*sizeof(double));
-    int yp[length(idx)+1];
     
-    int i;
-    double w0[ncells];
     int nl = length(idx);
+
+    int yp[nl+1];    
+    int w0[nfeature];
+    double x0[nfeature];
+    memset(w0, 0, sizeof(int)*nfeature);
+    memset(x0, 0, sizeof(double)*nfeature);
+
+    int i;
     for (i = 0; i < nl; ++i) { // foreach cell
+        if (n >= m - nfeature) {
+            m = m*2;
+            yi = realloc(yi, sizeof(int)*m);
+            yx = realloc(yx, sizeof(double)*m);
+        }
+
         yp[i] = n;
-        int ii = INTEGER(idx)[i];
-        memset(w0, 0, sizeof(double)*ncells);
-        int j, k;
-        // init weight array
-        for (k = wp[ii-1]; k < wp[ii]; ++k) {
-            w0[wi[k]] = wx[k];
+        int ii = INTEGER(idx)[i]-1;
+        int j;
+        for (j = wp[ii]; j < wp[ii+1]; ++j) {
+            n = scatter(x, wi[j], wx[j], w0, x0, ii+1, yi, n);
         }
-        
-        // foreach feature
-        for (j = 0; j < nfeature; ++j) {
-            double new = 0;
-            for (k = tp[j]; k < tp[j+1]; ++k) {
-                int idx = ti[k];
-                if (w0[idx] > 0) {
-                    new+=w0[idx] * tx[k];                    
-                }
-            }
-            if (new == 0) continue;
-            if (n == m) {
-                m = m*2;
-                yi = realloc(yi, sizeof(int)*m);
-                yx = realloc(yx, sizeof(double)*m);
-            }
-            yi[n] = j;
-            yx[n] = new;
-            n++;
-        }
+        for (j = yp[i]; j < n; ++j) yx[j] = x0[yi[j]];
     }
     yp[i] = n;
     
-    M_cholmod_free_sparse(&t, &c);
-    CHM_SP ans = M_cholmod_allocate_sparse(nfeature, nl, n, TRUE, TRUE, 0, CHOLMOD_REAL, &c);
+    CHM_SP ans = M_cholmod_allocate_sparse(nfeature, nl, n, FALSE, TRUE, 0, CHOLMOD_REAL, &c);
     if (ans == NULL) return(mkString("Failed to create sparse matrix"));
     int *ap = (int *)ans->p;
     int *ai = (int *)ans->i;
@@ -85,5 +87,6 @@ SEXP imputation1(SEXP _x, SEXP idx, SEXP _W)
     free(yi);
     free(yx);
 
+    M_cholmod_sort(ans, &c);
     return M_chm_sparse_to_SEXP(ans, 1, 0, 0, "N", R_NilValue);
 }
